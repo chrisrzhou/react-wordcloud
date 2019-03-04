48624dda1a1a9135ee37386c586c3a72f2300dd8
Chris Zhou <christoper.r.zhou@gmail.com>
2019-03-03 17:30:58 -0800

    update examples and core functionality

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,39 +1,84 @@
 import * as d3 from 'd3';
 import * as React from 'react';
+import ResizeObserver from 'resize-observer-polyfill';
 
-import { Selection } from './types';
+import { MinMaxPair, Selection } from './types';
 
 const { useEffect, useRef, useState } = React;
 
-export function useSvg<T>(
-  width: number,
-  height: number,
-): {
-  ref: React.RefObject<HTMLDivElement>;
-  selection: Selection;
-} {
-  const ref = useRef();
-  const [selection, setSelection] = useState();
+export function useResize(ref: React.RefObject<HTMLDivElement>): MinMaxPair {
+  const [size, setSize] = useState<MinMaxPair>([0, 0]);
 
   useEffect(() => {
-    setSelection(
-      d3
-        .select(ref.current)
-        .append('svg')
-        .attr('height', height)
-        .attr('width', width)
-        .append('g')
-        .attr('transform', `translate(${width / 2}, ${height / 2})`),
-    );
+    const element = ref.current;
+    const resizeObserver = new ResizeObserver(entries => {
+      if (!entries || !entries.length) {
+        return;
+      }
+      const { width, height } = entries[0].contentRect;
+      setSize([width, height]);
+    });
+    resizeObserver.observe(element);
+    return () => resizeObserver.unobserve(element);
+  }, []);
+  return size;
+}
+
+export function useResponsiveSVG<T>(
+  minSize: MinMaxPair,
+  initialSize?: MinMaxPair,
+): [React.RefObject<HTMLDivElement>, Selection, MinMaxPair] {
+  const ref = useRef();
+  const [selection, setSelection] = useState();
+  const [size, setSize] = useState(initialSize);
+  const resized = useResize(ref);
+
+  useEffect(() => {
+    const element = ref.current;
+    const {
+      offsetWidth: parentWidth,
+      offsetHeight: parentHeight,
+    } = element.parentNode;
+
+    // Update size with the following logic:
+    // - Use initialSize if it is provided
+    // - Use parentNode size if resized has not updated
+    // - Use resized when there are resize changes
+    // - Ensure that minSize is always applied/handled before updating size
+    let width;
+    let height;
+    if (initialSize !== undefined) {
+      [width, height] = initialSize;
+    } else {
+      if (resized[0] === 0 && resized[0] === 0) {
+        width = parentWidth;
+        height = parentHeight;
+      } else {
+        [width, height] = resized;
+      }
+    }
+    width = Math.max(width, minSize[0]);
+    height = Math.max(height, minSize[1]);
+    setSize([width, height]);
+
+    if (size !== undefined) {
+      setSelection(
+        d3
+          .select(element)
+          .append('svg')
+          .attr('height', height)
+          .attr('width', width)
+          .append('g')
+          .attr('transform', `translate(${width / 2}, ${height / 2})`),
+      );
+    }
+
     return () => {
       d3.select(ref.current)
         .selectAll('*')
         .remove();
     };
-  }, []);
+  }, [initialSize, initialSize === undefined && (resized[0] || resized[1])]);
 
-  return {
-    ref,
-    selection,
-  };
+  return [ref, selection, size];
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,120 @@
-export { default, defaultOptions } from './Wordcloud';
+import * as d3Cloud from 'd3-cloud';
+import * as React from 'react';
+import tippy from 'tippy.js';
+
+import { useResponsiveSVG } from './hooks';
+import render from './render';
+import { Callbacks, MinMaxPair, Options, Scale, Spiral, Word } from './types';
+import {
+  getDefaultColors,
+  getFontScale,
+  getText,
+  rotate,
+  TIPPY_CLASS,
+} from './utils';
+
+const { useEffect } = React;
+
+const d3 = { cloud: d3Cloud };
+
+export const defaultCallbacks: Callbacks = {
+  getWordTooltip: ({ count, text }: Word) => `${text} (${count})`,
+};
+
+export const defaultOptions: Options = {
+  colors: getDefaultColors(),
+  enableTooltip: true,
+  fontFamily: 'impact',
+  fontSizes: [5, 40],
+  fontStyle: 'normal',
+  fontWeight: 'normal',
+  padding: 1,
+  rotationAngles: [-90, 90],
+  rotations: undefined,
+  scale: Scale.Sqrt,
+  spiral: Spiral.Archimedean,
+  transitionDuration: 600,
+};
+
+interface Props {
+  callbacks: Callbacks;
+  minSize: MinMaxPair;
+  maxWords: number;
+  options: Options;
+  size: MinMaxPair;
+  words: Word[];
+}
+
+function Wordcloud({
+  callbacks,
+  maxWords,
+  options,
+  minSize,
+  size: initialSize,
+  words,
+}: Props): React.ReactNode {
+  const [ref, selection, size] = useResponsiveSVG(minSize, initialSize);
+  const layout = d3.cloud();
+
+  // render viz
+  useEffect(() => {
+    if (selection && size) {
+      const {
+        enableTooltip,
+        fontFamily,
+        fontStyle,
+        fontSizes,
+        fontWeight,
+        rotations,
+        rotationAngles,
+        spiral,
+        scale,
+      } = options;
+
+      if (enableTooltip) {
+        tippy(`.${TIPPY_CLASS}`);
+      }
+
+      const sortedWords = words
+        .concat()
+        .sort()
+        .slice(0, maxWords);
+
+      if (rotations !== undefined) {
+        layout.rotate(() => rotate(rotations, rotationAngles));
+      }
+
+      layout
+        .size(size)
+        .padding(1)
+        .words(words)
+        .spiral(spiral)
+        .text(getText)
+        .font(fontFamily)
+        .fontSize((word: Word) => {
+          const fontScale = getFontScale(words, fontSizes, scale);
+          return fontScale(word.count);
+        })
+        .fontStyle(fontStyle)
+        .fontWeight(fontWeight)
+        .on('end', () => {
+          // merge options and callbacks to handle missing values
+          const mergedCallbacks = { ...defaultCallbacks, ...callbacks };
+          const mergedOptions = { ...defaultOptions, ...options };
+          render(selection, sortedWords, mergedOptions, mergedCallbacks);
+        })
+        .start();
+    }
+  }, [options, maxWords, selection, size, words]);
+
+  return <div ref={ref} />;
+}
+
+Wordcloud.defaultProps = {
+  callbacks: defaultCallbacks,
+  maxWords: 100,
+  minSize: [200, 150],
+  options: defaultOptions,
+};
+
+export default Wordcloud;
