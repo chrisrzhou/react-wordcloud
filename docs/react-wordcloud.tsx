89149f837016774e83c8ee7ce35ab3b6877fca0f
David Beitey <david@davidjb.com>
2020-08-14 18:03:56 +1000

    Rework canvas access warning
    The previous implementation was checking whether a setting like
    resistfingerprinting was preventing access to the browser's performance
    API.  However, it's possible to have resistfingerprinting enabled but
    still enable canvas access on a site-by-site basis (e.g. the browser
    prompts for permission upon first access if resistfingerprinting is
    enabled).
    
    This commit changes the access check to directly examine the result of
    trying to extract data from a fake, blank canvas. If anything but zeroes
    appear when checking data, such as the random integers Firefox returns,
    then it can safely determined that the browser is blocking canvas
    access.
    
    For the wordcloud homepage, this means that because a timer is employed,
    enabling the canvas access immediately sees the wordcloud starting to
    work, without even needing to reload.
    
    Ref: #44

--- docs/react-wordcloud.tsx
+++ docs/react-wordcloud.tsx
@@ -8,18 +8,13 @@ import ReactWordcloudSrc, { Props } from '..';
 export * from '..';
 
 export default function ReactWordcloud(props: Props): JSX.Element {
-  let entries = [];
+  const canvasAllowed = typeof document !== 'undefined' && document.createElement('canvas').getContext('2d').getImageData(0, 0, 1, 1).data.every(v => v === 0);
 
-  if (typeof window !== 'undefined') {
-    window.performance.mark('check');
-    entries = window.performance.getEntries();
-  }
-
-  if (entries.length === 0) {
+  if (!canvasAllowed) {
     return (
       <p>
         React wordcloud requires access to canvas image data. Please allow
-        access in your browser and try again.
+        access in your browser and reload the page.
       </p>
     );
   }
