691a92c1483441b4181c4ad586873459dceb75fd
Thiago Ventura <serraventura@gmail.com>
2022-11-05 13:12:26 +0000

    testing exports

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-// export { default } from './src';
+export { default } from './src';
 
-// export * from './src';
+export * from './src';
 
 
-export default {
-    input: './src/index.js',
-    output: './dist/index.js',
-    format: 'esm',
-    exports: 'named',
-    sourcemap: true,
-};
+// export default {
+//     input: './src/index.js',
+//     output: './dist/index.js',
+//     format: 'esm',
+//     exports: 'named',
+//     sourcemap: true,
+// };
