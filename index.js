79ce2510a2437ffe95468d71d2be2abbfdbcf763
Thiago Ventura <serraventura@gmail.com>
2022-11-05 13:30:47 +0000

    clean up

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
 export { default } from './src';
 
 export * from './src';
-
-
-// export default {
-//     input: './src/index.js',
-//     output: './dist/index.js',
-//     format: 'esm',
-//     exports: 'named',
-//     sourcemap: true,
-// };
