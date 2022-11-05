6ce68b325095aad20fa87618c2806ce31a813873
Thiago Ventura <serraventura@gmail.com>
2022-11-05 13:03:03 +0000

    testing react 18

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-export { default } from './src';
+// export { default } from './src';
 
-export * from './src';
+// export * from './src';
+
+
+export default {
+    input: './src/index.js',
+    output: './dist/index.js',
+    format: 'esm',
+    exports: 'named',
+    sourcemap: true,
+};
